b9d21fc806990543ac164cc74034bbd5611fa660
ambishalsaha01 <bishalsaha9989@gmail.com>
2022-05-02 07:42:34 -0700

    implement header and header section

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,8 +4,8 @@ import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../../firebase.init';
-import logo from '../../../images/logo.png'
-import './Header.css'
+import logo from '../../../images/logo.png';
+import './Header.css';
 
 const Header = () => {
     const [user] = useAuthState(auth);
